5a0c60356106e157bb47c59598729b789980e70a
Rouven Spaar <rouven.spaar@gmail.com>
2024-10-15 20:59:19 +0200

    update: navBar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,18 +1,22 @@
 import Link from 'next/link';
 import styles from './navBar.module.scss';
-import { Button } from './ui/button';
+import {Button} from './ui/button';
 
 export default function NavBar() {
     return (
+        <div className={styles.navBarWrapper}>
         <div className={styles.navBar}>
-            <div className={styles.logo}>AstroLog</div>
-            <div className={styles.navigation}>
-                <Link href='/'>Product</Link>
-                <Link href='/purchase'>Purchase</Link>
-                <Link href='/downloads'>Downloads</Link>
-                <Link href='/about'>About</Link>
+            <div className={styles.left}>
+                <div className={styles.logo}>AstroLog</div>
+                <div className={styles.navigation}>
+                    <Link href='/'>Product</Link>
+                    <Link href='/purchase'>Purchase</Link>
+                    <Link href='/downloads'>Downloads</Link>
+                    <Link href='/about'>About</Link>
+                </div>
             </div>
             <Button>Purchase</Button>
+        </div>
         </div>
     );
 }
